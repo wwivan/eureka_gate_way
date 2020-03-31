3ce8b229bfcf234db6aac4c7eb3c738b00bc666a
wwivan <1024528728@qq.com>
2020-03-31 17:21:19 +0800

    init

---
+++ Gate_way/src/main/java/gate_way/MainApplication.java
@@ -0,0 +1,13 @@
+package gate_way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class);
+    }
+}
---
+++ Server/Test_server/src/main/java/server/controller/TestController.java
@@ -0,0 +1,16 @@
+package server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @ResponseBody
+    @GetMapping("/test")
+    public String test(){
+        return "hello";
+    }
+}
